7941485b2042979d5ce77259bf324aa11836e72c
Elizaveta <mefmag01@gmail.com>
2025-11-07 13:49:20 +0000

    добавлен import pytest , добавлены тесты со случаями исключениями

--- test/test_bin_searh.py
+++ test/test_bin_searh.py
@@ -1,3 +1,4 @@
+import pytest
 from src.bin_search import binSearch
 
 
@@ -11,3 +12,13 @@ def test_start():
 
 def test_not_in_list():
     assert binSearch([1, 2, 3, 4], 5) == -1
+
+
+def test_unsorted():
+    with pytest.raises(ValueError, match="Массив не отсортирован"):
+        binSearch([8, 3, 6, 1], 6)
+
+
+def test_unsorted():
+    with pytest.raises(ValueError, match="такого элемента нет"):
+        binSearch([1, 2, 3], 9)
